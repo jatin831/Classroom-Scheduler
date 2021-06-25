7bcb6937f7b45a77c154078d30f12fe0a3872606
jatin831 <jatinbajaj2001@gmail.com>
2021-06-25 21:01:26 +0530

    dbms made online

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.use(cors())
 
 const pool = mysql.createPool({
     connectionLimit     : 5,
-    host                : 'localhost',
-    user                : 'root',
-    password            : 'password',
-    database            : 'scheduler'
+    host                : 'bxjk335sley77gqee21r-mysql.services.clever-cloud.com',
+    user                : 'un8cfhvh7kyxkggm',
+    password            : 'SmKx9AgcZGuQgrSYiF73',
+    database            : 'bxjk335sley77gqee21r'
 })
 
 app.get('/api/teachers', (req, res) => {
