c89e3f3dc70439cdd0bbfb71c74620db2461342f
jatin831 <jatinbajaj2001@gmail.com>
2021-06-25 21:31:18 +0530

    bug removed

--- app.js
+++ app.js
@@ -255,7 +255,7 @@ app.delete("/api/deleteTeacher", (req, res) => {
     })
 })
 
-if ( process.env.NODE_ENV !== "production") { 
+if ( process.env.NODE_ENV == "production") { 
     app.use(express.static("client/build")); 
     const path = require("path"); app.get("*", (req, res) => { 
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
