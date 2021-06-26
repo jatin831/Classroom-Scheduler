20e11135e2bedc36d746556f3053674b037cc4ca
jatin831 <jatinbajaj2001@gmail.com>
2021-06-26 21:41:27 +0530

    m

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ app.delete("/api/deleteTeacher", (req, res) => {
 })
 
 if ( process.env.NODE_ENV == "production" || 1) { 
-    app.use(express.static("client/build")); 
+    app.use(express.static("client2/build")); 
     const path = require("path"); app.get("*", (req, res) => { 
         res.sendFile(path.resolve(__dirname, 'client2', 'build', 'index.html')); 
     })
